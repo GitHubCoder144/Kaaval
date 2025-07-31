18ca46276a9f30cacf75d856c72d3bf7fde79a4b
GitHubCoder144 <johanbabu200@gmail.com>
2025-07-31 13:36:59 -0700

    added main and comments

--- core/downlwatch.py
+++ core/downlwatch.py
@@ -17,3 +17,24 @@ class FileScanner(FileSystemEventHandler):
 
         filescan(filepath)
 
+def main():
+    downloads_path = os.path.expanduser("~/Downloads")
+    print(f"Kavval starting watch on: {downloads_path}")
+
+    event_handler = FileScanner()
+
+    observer = Observer()
+
+    observer.schedule(event_handler, path=downloads_path, recursive=True)
+
+    observer.start()
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
+
+if __name__ == "__main__":
+    main()
